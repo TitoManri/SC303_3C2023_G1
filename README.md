558f786d835ec8b800c8f94e95271ca1b64a291b
TitoManri <manri.carazo@gmail.com>
2023-10-06 12:29:13 -0600

    Creacion de la plantilla

---
+++ README.md
@@ -0,0 +1,2 @@
+# SC303_3C2023_G1
+Proyecto de un parque de diversiones para la materia de Programacion Cliente/Servidor en la Universidad Fidelitas
---
+++ src/main/java/org/yourcompany/yourproject/SC303_3C2023_G1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author manri
+ */
+public class SC303_3C2023_G1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
