9c2a206e520fc3d6dc4c03832104577afc6e01e8
Tito <61942255+TitoManri@users.noreply.github.com>
2023-11-23 14:26:54 -0600

    .
    .

--- SC303_3C2023_G1/src/main/java/Jframe/Administrador/Atracciones/CatalogoAtracciones.java
+++ SC303_3C2023_G1/src/main/java/Jframe/Administrador/Atracciones/CatalogoAtracciones.java
@@ -36,37 +36,29 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        actualizarCategorias = new javax.swing.JButton();
         limpiar = new javax.swing.JButton();
         nombreAtraccionTexto = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        empleadoTexto = new javax.swing.JTextField();
+        categoriasTexto = new javax.swing.JTextField();
         agregarAtraccionBoton = new javax.swing.JButton();
-        categoriasComboBox = new javax.swing.JComboBox<>();
         jScrollPane2 = new javax.swing.JScrollPane();
         empleadosDisponibles = new javax.swing.JTextArea();
         jLabel5 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         volverACatalogo = new javax.swing.JButton();
-        labelTelefono6 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         editarAtraccion = new javax.swing.JButton();
-        inactivarAtracciones = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        categoriasCuadro1 = new javax.swing.JTextArea();
+        jLabel8 = new javax.swing.JLabel();
+        empleadoTexto1 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximizedBounds(new java.awt.Rectangle(0, 0, 0, 0));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        actualizarCategorias.setText("Actualizar Categorias");
-        actualizarCategorias.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                actualizarCategoriasActionPerformed(evt);
-            }
-        });
-        getContentPane().add(actualizarCategorias, new org.netbeans.lib.awtextra.AbsoluteConstraints(345, 377, -1, -1));
 
         limpiar.setText("Limpiar");
         limpiar.addActionListener(new java.awt.event.ActionListener() {
@@ -74,29 +66,29 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
                 limpiarActionPerformed(evt);
             }
         });
-        getContentPane().add(limpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 511, 84, -1));
+        getContentPane().add(limpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 370, 84, -1));
 
         nombreAtraccionTexto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 nombreAtraccionTextoActionPerformed(evt);
             }
         });
-        getContentPane().add(nombreAtraccionTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 255, 278, -1));
+        getContentPane().add(nombreAtraccionTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 310, 278, -1));
 
         jLabel3.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         jLabel3.setText("Nombre Atraccion");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(95, 231, -1, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 280, -1, -1));
 
         jLabel4.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
         jLabel4.setText("Empleado");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(115, 424, -1, -1));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 280, -1, -1));
 
-        empleadoTexto.addActionListener(new java.awt.event.ActionListener() {
+        categoriasTexto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                empleadoTextoActionPerformed(evt);
+                categoriasTextoActionPerformed(evt);
             }
         });
-        getContentPane().add(empleadoTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 460, 278, -1));
+        getContentPane().add(categoriasTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 310, 220, -1));
 
         agregarAtraccionBoton.setText("Agregar Atraccion");
         agregarAtraccionBoton.addActionListener(new java.awt.event.ActionListener() {
@@ -104,29 +96,22 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
                 agregarAtraccionBotonActionPerformed(evt);
             }
         });
-        getContentPane().add(agregarAtraccionBoton, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 511, -1, -1));
-
-        categoriasComboBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                categoriasComboBoxActionPerformed(evt);
-            }
-        });
-        getContentPane().add(categoriasComboBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 377, 292, -1));
+        getContentPane().add(agregarAtraccionBoton, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 370, -1, -1));
 
         empleadosDisponibles.setEditable(false);
         empleadosDisponibles.setColumns(20);
         empleadosDisponibles.setRows(5);
         jScrollPane2.setViewportView(empleadosDisponibles);
 
-        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 68, -1, 145));
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 70, -1, 145));
 
         jLabel5.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        jLabel5.setText("Categorias");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(113, 339, -1, -1));
+        jLabel5.setText("Categoria");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 280, -1, -1));
 
         jLabel1.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        jLabel1.setText("Empleados Disponibles");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 30, -1, -1));
+        jLabel1.setText("Categorias");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, -1, -1));
 
         volverACatalogo.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
         volverACatalogo.setText("Volver a Catalogo");
@@ -135,19 +120,15 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
                 volverACatalogoActionPerformed(evt);
             }
         });
-        getContentPane().add(volverACatalogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 528, -1, -1));
+        getContentPane().add(volverACatalogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 490, -1, -1));
 
-        labelTelefono6.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
-        labelTelefono6.setText("->");
-        getContentPane().add(labelTelefono6, new org.netbeans.lib.awtextra.AbsoluteConstraints(316, 375, -1, -1));
+        jLabel2.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
+        jLabel2.setText("Editar e Inactivar Atraccion");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 500, -1, -1));
 
-        jLabel2.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        jLabel2.setText("Editar Atraccion");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(611, 223, -1, -1));
-
-        jLabel6.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        jLabel6.setText("Inactivar Atraccion");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(602, 312, -1, -1));
+        jLabel6.setFont(new java.awt.Font("Helvetica Neue", 0, 28)); // NOI18N
+        jLabel6.setText("->");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 490, -1, -1));
 
         editarAtraccion.setText("Editar");
         editarAtraccion.addActionListener(new java.awt.event.ActionListener() {
@@ -155,50 +136,51 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
                 editarAtraccionActionPerformed(evt);
             }
         });
-        getContentPane().add(editarAtraccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(625, 263, 84, -1));
-
-        inactivarAtracciones.setText("Inactivar");
-        inactivarAtracciones.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                inactivarAtraccionesActionPerformed(evt);
-            }
-        });
-        getContentPane().add(inactivarAtracciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 348, 84, -1));
+        getContentPane().add(editarAtraccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 500, 84, -1));
 
         jLabel7.setFont(new java.awt.Font("Helvetica Neue", 0, 24)); // NOI18N
         jLabel7.setText("Catalogo Atracciones");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 70, -1, -1));
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 80, -1, -1));
+
+        categoriasCuadro1.setEditable(false);
+        categoriasCuadro1.setColumns(20);
+        categoriasCuadro1.setRows(5);
+        jScrollPane3.setViewportView(categoriasCuadro1);
+
+        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, -1, 145));
+
+        jLabel8.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        jLabel8.setText("Empleados Disponibles");
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 30, -1, -1));
+
+        empleadoTexto1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                empleadoTexto1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(empleadoTexto1, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 310, 230, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void actualizarCategoriasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_actualizarCategoriasActionPerformed
-        DefaultComboBoxModel<String> comboBoxModel = (DefaultComboBoxModel<String>) categoriasComboBox.getModel();
-        Categorias c = new Categorias();
-        ArrayList<Categorias> categoria = c.getCategoria();
-        for (Categorias categorias : categoria) {
-            comboBoxModel.addElement(categorias.getNombreCategorias());
-        }
-    }//GEN-LAST:event_actualizarCategoriasActionPerformed
-
     private void limpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limpiarActionPerformed
         nombreAtraccionTexto.setText(null);
-        empleadoTexto.setText(null);
+        categoriasTexto.setText(null);
     }//GEN-LAST:event_limpiarActionPerformed
 
     private void nombreAtraccionTextoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nombreAtraccionTextoActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_nombreAtraccionTextoActionPerformed
 
-    private void empleadoTextoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_empleadoTextoActionPerformed
+    private void categoriasTextoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_categoriasTextoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_empleadoTextoActionPerformed
+    }//GEN-LAST:event_categoriasTextoActionPerformed
 
     private void agregarAtraccionBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregarAtraccionBotonActionPerformed
         try{
         BufferedWriter archivo = new BufferedWriter(new FileWriter("src/main/java/BaseDeDatos/Atracciones.txt", true));   
         String nombreAtraccion = nombreAtraccionTexto.getText();
-        String empleadoAtraccion = empleadoTexto.getText();
+        String empleadoAtraccion = categoriasTexto.getText();
         boolean estadoCategorias = true;
         
         if ((nombreAtraccion.equals(""))||(empleadoAtraccion.equals(""))) {
@@ -224,10 +206,6 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
 
     }//GEN-LAST:event_agregarAtraccionBotonActionPerformed
 
-    private void categoriasComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_categoriasComboBoxActionPerformed
-
-    }//GEN-LAST:event_categoriasComboBoxActionPerformed
-
     private void volverACatalogoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_volverACatalogoActionPerformed
         Catalogos catalogos = new Catalogos();
         catalogos.setVisible(true);
@@ -240,9 +218,9 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_editarAtraccionActionPerformed
 
-    private void inactivarAtraccionesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inactivarAtraccionesActionPerformed
+    private void empleadoTexto1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_empleadoTexto1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_inactivarAtraccionesActionPerformed
+    }//GEN-LAST:event_empleadoTexto1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -295,13 +273,12 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton actualizarCategorias;
     private javax.swing.JButton agregarAtraccionBoton;
-    private javax.swing.JComboBox<String> categoriasComboBox;
+    private javax.swing.JTextArea categoriasCuadro1;
+    private javax.swing.JTextField categoriasTexto;
     private javax.swing.JButton editarAtraccion;
-    private javax.swing.JTextField empleadoTexto;
+    private javax.swing.JTextField empleadoTexto1;
     private javax.swing.JTextArea empleadosDisponibles;
-    private javax.swing.JButton inactivarAtracciones;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -309,8 +286,9 @@ public class CatalogoAtracciones extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JLabel labelTelefono6;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JButton limpiar;
     private javax.swing.JTextField nombreAtraccionTexto;
     private javax.swing.JButton volverACatalogo;
